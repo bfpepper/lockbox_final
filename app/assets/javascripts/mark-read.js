57099641802c8abe1cd2fd34638263c4b184f04b
Ben Pepper <bfpepper87@gmail.com>
2017-02-27 14:06:35 -0700

    initial commit.

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -5,11 +5,12 @@ $(document).ready(function(){
   $('#links-list').on('click', 'button.mark-read', function(){
     var $this = $(this);
     var linkId = $this.parents('.link').data('id');
-    
+
     $.ajax({
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: true}
+      
     });
   })
 })
